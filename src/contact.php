8605889253ddc22003c0a25f1114927db46adacc
ashkezar <ashk3zar@gmail.com>
2014-03-28 21:49:14 +0430

    routing system

---
+++ src/contact.php
@@ -0,0 +1,18 @@
+<?php
+
+class contact
+{
+	public function __construct()
+	{
+		echo 'This is the Contact page'; 
+	}
+	
+}
+class add
+{
+	public function __construct()
+	{
+		echo 'Add contact'; 
+	}
+	
+}
---
+++ src/login.php
@@ -0,0 +1,19 @@
+<?php
+
+class login
+{
+	public function __construct()
+	{
+		echo 'This is the LOGIN page'; 
+	}
+	
+}
+
+class loginuser
+{
+	public function __construct()
+	{
+		echo 'This is the LOGIN !!!!'; 
+	}
+	
+}
