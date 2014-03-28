cb5ebe703eacce71cbe18b89e06753f6116cb645
ashkezar <ashk3zar@gmail.com>
2014-03-28 21:43:43 +0430

    سیستم جداسازی و ایجاد هر صفحه. به اصطلاح routing system

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
-echo "Hello Tortoise!";
-echo "hello too"
-?>
+
+include 'route.php';
+include 'src/about.php';
+include 'src/home.php';
+include 'src/contact.php';
+include 'src/login.php';
+
+
+
+$route = new Route();
+
+$route->add('/', function() {
+	echo 'Hey this is home';
+});
+
+$route->add('/about', 'About');
+$route->add('/contact', 'Contact');
+$route->add('/contact/add', 'add');
+$route->add('/login', 'Login');
+$route->add('/login/user', 'add');
+
+// echo '<pre>';
+// print_r($route);
+
+$route->submit();
