b5720008efc6c9f6e238daafc8880b02736bb006
ashkezar <ashk3zar@gmail.com>
2014-04-14 22:07:11 +0430

    SQL code To create DB tables and coloumns

--- src/home.php
+++ src/home.php
@@ -7,9 +7,17 @@ class Home
 		echo 'This is the home page'; 
 
 		//$host  = $_SERVER['HTTP_HOST'];
-		header("Location: login");
-		
+		//header("Location: login");
 		//$this->_other();
+
+		session_start(); // Start the session.
+
+		// If no session value is present, redirect the user:
+		// Also validate the HTTP_USER_AGENT!
+	if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
+	header("Location: login");
+	exit();	
+}
 	}
 	
 	protected function _other()
