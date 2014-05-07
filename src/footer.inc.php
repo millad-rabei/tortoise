69538edcacc0a217ca22e6a7bb3f8bf9405e8848
Mirdehghan <mojtaba.smd@gmail.com>
2014-05-07 10:50:23 +0430

    1393

--- src/footer.inc.php
+++ src/footer.inc.php
@@ -1,5 +1,5 @@
 <div class="footer-wrap">
-<div class="footer">سیستم اتوماسیون اداری سنگ پشت | نسخه 1 | بهار 1392</div>
+<div class="footer">سیستم اتوماسیون اداری سنگ پشت | نسخه 1 | بهار 1393</div>
 </div>
 
 </body>
