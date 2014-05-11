7ea00f125dbe6709227e3473a010d7777397c6ce
Mirdehghan <ashkezar@yahoo.com>
2014-05-11 20:34:14 +0430

    add ...

---
+++ src/user.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class User
+{
+	public function __construct()
+	{
+		session_start(); // Start the session.
+		$pagetitle = "مدیریت پرسنل";
+
+		// If no session value is present, redirect the user:
+		// Also validate the HTTP_USER_AGENT!
+	if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
+	//redirect to login page
+	echo '<script> window.location="login"; </script>';
+	exit();	
+}
+else{
+
+		include 'header.inc.php';
+		include 'topheader.inc.php';
+		include 'main.inc.php';
+		include 'dashboard-primary-tools.php';
+		include 'dashboard-content.php';
+		include 'dashboard-secondary-tools.php';
+		include 'footer.inc.php';
+}
+	}
+
+}
