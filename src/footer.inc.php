b714d925ee3fa69db218d9d8870455eb35952afa
Mirdehghan <ashkezar@yahoo.com>
2014-05-12 21:35:55 +0430

    ...

--- src/footer.inc.php
+++ src/footer.inc.php
@@ -1,5 +1,5 @@
 <div class="footer-wrap">
-<div class="footer">سیستم اتوماسیون اداری سنگ پشت | نسخه 1 | بهار 1393</div>
+<div class="footer">سیستم اتوماسیون اداری سنگ پشت | نسخه 0.1 | بهار 1393</div>
 </div>
 
 </body>
