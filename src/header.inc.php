e7e1712c4adc7b92397eee635883b8191279f6ef
Mirdehghan <ashkezar@yahoo.com>
2014-05-13 14:22:55 +0430

    add favicon

--- src/header.inc.php
+++ src/header.inc.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8" >
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" type="image/png" href="favicon.ico">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
 <title><?php echo $pagetitle; ?> :: اتوماسیون اداری سنگ پشت</title>
