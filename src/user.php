19738d96ac848f8364c3cac0a33c9cfb25211804
Mirdehghan <ashkezar@yahoo.com>
2014-05-31 23:52:39 +0430

    ...

--- src/user.php
+++ src/user.php
@@ -44,7 +44,87 @@ class manageuser
 	exit();	
 }
 else{
+		include 'header.inc.php';
+		include 'topheader.inc.php';
+		include 'main.inc.php';
+		include 'user-primary-tools.php';
+		include 'user-content.php';
+		include 'user-secondary-tools.php';
+		include 'footer.inc.php';
+}
+	}
 
+}
+
+class managetitle
+{
+	public function __construct()
+	{
+		session_start(); // Start the session.
+		$pagetitle = "مدیریت سمت ها";
+
+		// If no session value is present, redirect the user:
+		// Also validate the HTTP_USER_AGENT!
+	if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
+	//redirect to login page
+	echo '<script> window.location="../login"; </script>';
+	exit();	
+}
+else{
+		include 'header.inc.php';
+		include 'topheader.inc.php';
+		include 'main.inc.php';
+		include 'user-primary-tools.php';
+		include 'user-content.php';
+		include 'user-secondary-tools.php';
+		include 'footer.inc.php';
+}
+	}
+
+}
+
+class managegroup
+{
+	public function __construct()
+	{
+		session_start(); // Start the session.
+		$pagetitle = "مدیریت گروه ها";
+
+		// If no session value is present, redirect the user:
+		// Also validate the HTTP_USER_AGENT!
+	if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
+	//redirect to login page
+	echo '<script> window.location="../login"; </script>';
+	exit();	
+}
+else{
+		include 'header.inc.php';
+		include 'topheader.inc.php';
+		include 'main.inc.php';
+		include 'user-primary-tools.php';
+		include 'user-content.php';
+		include 'user-secondary-tools.php';
+		include 'footer.inc.php';
+}
+	}
+
+}
+
+class managepermission
+{
+	public function __construct()
+	{
+		session_start(); // Start the session.
+		$pagetitle = "مدیریت حق دسترسی ها";
+
+		// If no session value is present, redirect the user:
+		// Also validate the HTTP_USER_AGENT!
+	if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
+	//redirect to login page
+	echo '<script> window.location="../login"; </script>';
+	exit();	
+}
+else{
 		include 'header.inc.php';
 		include 'topheader.inc.php';
 		include 'main.inc.php';
