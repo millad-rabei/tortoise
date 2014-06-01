e136e302e6a37d1e669a05a0b03b606bd5077526
masood tayari <masoodt23@gmail.com>
2014-06-01 13:23:18 +0430

    Masood tayari

---
+++ tayari/erja.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head> <meta charset="UTF-8"> </head>
+<title>ارجاع مدارک</title>
+<body>
+<center><h1> ... ارجاع مدرک با شماره ... و با عنوان</h1></center>
+<h2 align="right" >ارجاع مدرک</h2>
+<form action="home.php" method="post">
+<fieldset align="right" ><legend align="right" >....:نام پرسنل</legend><fieldset align="right" > <fieldset>
+<input align="right" type="radio" name="r1" value="yes" >قابلیت پیگیری</input><input align="right" type="radio" name="r1" value="yes" >قفل نمودن</input>
+</fieldset>
+<br>
+<table align="right" border="4" ><tr>
+<td width="600" height="300" ><fieldset align="right" ><table align="right" border="2" >
+<tr><td width="150" align="center" >توضیح شخصی</td>
+<td width="150" align="center" >دستور ارجاع</td>
+<td width="150" align="center" >نوع عمل</td>
+<td width="150" align="center" >گیرنده مدرک</td>
+<td>ردیف</td></tr>
+<tr height="200" ><td></td><td></td><td></td><td></td><td></td></tr>
+
+</table></fieldset>
+
+<input type="submit" value="تنظیمات ارجاع">
+<input type="submit" value="حذف گیرندگان"></td>
+<td width="600" height="300" ><fieldset ><legend align="right" >انتخاب پرسنل</legend>
+<br><br><br><br><br><br><br><br><br><br></fieldset>
+<input type="submit" value="افزودن">
+<input type="text" name="fname">
+<select name="cars">
+            <option value="include">شامل</option>
+             </select>
+<select name="cars">
+            <option value="semat">سمت</option>
+             </select></td>
+</tr></table>
+<br>
+<br>
+<br><br><br><br><br>
+
+
+
+</fieldset> 
+<center>
+<input type="submit" value="بستن پنجره">
+<input type="submit" value="ارجاع">
+</center>
+</fieldset> 
+</form>
+
+<input type="submit" value="ویرایش">
+<input type="submit" value="چرخه اجرا">
+<input type="submit" value="چاپ">
+
+</body>
+
+</html>
