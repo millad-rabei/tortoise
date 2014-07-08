6e0eab6b221aef0771ac3aec419258fc54a67c59
Zebhi <fz.ashkezari@yahoo.com>
2014-07-08 09:27:08 +0430

    Delete All Pro

--- src/dashboard-primary-tools.php
+++
@@ -1,8 +0,0 @@
-	<div class="primary-tools">
-	<ul class="prtools">
-		<a href="http://nakhlesefid.com"><li>ایجاد نامه</li></a>
-		<a href="http://nakhlesefid.com"><li>جستجو</li></a>
-		<a href="http://nakhlesefid.com"><li>Add</li></a>
-		<a href="http://nakhlesefid.com"><li>Add</li></a>
-	</ul>
-	</div>
--- src/user-content.php
+++
@@ -1,5 +0,0 @@
-	<div class="content">
-		<p></p>
-
-		
-	</div>
