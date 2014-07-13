aebda88e3b6d0622ed4243ca64b56ec2712f296d
Mirdehghan <mojtaba.smd@gmail.com>
2014-07-13 13:35:41 +0430

    ..

--- form.class.php
+++ form.class.php
@@ -68,21 +68,41 @@ class form{
 		public function adduser(){
 		echo '
 			<form id="pgt-form-adduser" method="post" action="../run-query-pgt.php">
-			<div class="containli"><label>نام کاربری : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>رمز عبور : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>تکرار رمز عبور : </label><input name="lastname" type="text" class="pgt"></div></br>
+			<div class="containli"><label>نام کاربری : </label><input style="direction:LTR;" name="username" type="text" class="pgt"></div></br>
+			<div class="containli"><label>رمز عبور : </label><input style="direction:LTR;" name="password" type="password" class="pgt"></div></br>
+			<div class="containli"><label>تکرار رمز عبور : </label><input style="direction:LTR;" name="confirm-password" type="password" class="pgt"></div></br>
 			<div class="containli"><label>نام : </label><input name="firstname" type="text" class="pgt"></div></br>
 			<div class="containli"><label>نام خانوادگی : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>فایل امضاء : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>کدملی : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>تلفن همراه : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>تاریخ تولد : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>آدرس : </label><input name="lastname" type="text" class="pgt"></div></br>
-			<div class="containli"><label>جانشنین : </label><input name="lastname" type="text" class="pgt"></div></br>
+			<div class="containli"><label>فایل امضاء : </label><input style="direction:LTR;" name="signature" type="file" class="pgt"></div></br>
+			<div class="containli"><label>کدملی : </label><input style="direction:LTR;" name="melicode" type="text" class="pgt"></div></br>
+			<div class="containli"><label>تلفن همراه : </label><input style="direction:LTR;" name="cellphone" type="text" class="pgt"></div></br>
+			<div class="containli"><label>تاریخ تولد : </label><input style="direction:LTR;" pattern="[A-Za-z]{3}" name="birthdate" type="text" class="pgt"></div></br>
+			<div class="containli"><label>آدرس : </label><input name="address" type="text" class="pgt"></div></br>
+			<div class="containli"><label>جانشنین : </label>
+			<select class="pgt" >
+				<option value="volvo">Volvo</option>
+				<option value="saab">Saab</option>
+				<option value="mercedes">Mercedes</option>
+				<option value="audi">Audi</option>
+			</select> 
+			</div></br>
+		<div class="containselect">
+			<div class="containli"><label>انتخاب گروه ها : </label>
+			<select class="pgt" >
+				<option value="volvo">Volvo</option>
+				<option value="saab">Saab</option>
+				<option value="mercedes">Mercedes</option>
+				<option value="audi">Audi</option>
+			</select>
+			</div>
+						<input name="submit" class="button" type="submit" value="ثبت"> 
+		</div>
+			</br>
 			<input name="whatwork" type="hidden" value="adduser">
 			<input type="hidden" name="id" value="">
 			<input id="section" name="section" type="hidden" value="'.$this->section.'">
-			<div class="containli"><input name="submit" class="button" type="submit" value="ثبت"></div>
+			<div class="containli"><input name="submit" class="button" type="submit" value="ثبت">
+			<input class="button" type="reset" value="از نو"></div>
 			</form>
 		';
 		//close the pop up
